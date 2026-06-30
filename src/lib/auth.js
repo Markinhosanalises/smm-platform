6ed15f19d2c276814604f608d49964506f3e1d81
Markinhosanalises <marcosprimeconsultas@gmail.com>
2026-06-29 22:42:56 -0300

    Delete src directory

--- src/lib/auth.js
+++
@@ -1,3 +0,0 @@
-function requireAuth(req,res,next){ if(!req.session.user) return res.redirect('/login'); next(); }
-function requireAdmin(req,res,next){ if(!req.session.user||req.session.user.role!=='admin') return res.redirect('/login'); next(); }
-module.exports={requireAuth,requireAdmin};
